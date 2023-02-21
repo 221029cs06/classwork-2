6c29cc866d05aac2c7dbef09168f2ed3d7f46bcb
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-21 15:19:28 +0500

    Add files via upload

---
+++ problem_1.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  problem_1
+//
+//  Created by Asilbek Alimov on 21/02/23.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cin>>x;
+    if(x>0){
+        cout<<"Positive"<<endl;
+    }else{
+        cout<<"Negative";
+    }
+    return 0;
+}
